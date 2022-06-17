772eb5358c3a94a445379c113c9c21fc94cce8bc
Shouryam Sankritya <shouryam16@gmail.com>
2022-06-18 00:38:43 +0530

    commit changes

---
+++ Arrays/Array_implementation.cpp
@@ -0,0 +1,12 @@
+// Implementation of Arrays :-
+#include<bits/stdc++.h>     //Header files initialization
+#include<iostream>          //Header files initialization
+using namespace std;
+    int main(){
+    int array[10] = {1,2,3,4,5,6,7,8,9,10};     //Array Declaration
+    int i;
+        for(i = 0; i < 10; i++){
+            std::cout<<array[i]<<" ";           //Displaying Array
+        }
+return 0;
+        }
